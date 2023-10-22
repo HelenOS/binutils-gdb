8c033929241a463d986b51f5c4fb1b244a453acd
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2023-10-22 23:28:28 +0200

    Go back to 'squatting' in other platform's arm/aarch64 definitions
    There's no value to keeping separate files for us, and it's less
    cognitive effort to update the port this way.

--- ld/emulparams/aarch64helenos.sh
+++
@@ -1,7 +0,0 @@
-. ${srcdir}/emulparams/aarch64elf.sh
-
-unset EMBEDDED
-unset STACK_ADDR
-
-# Ensure each PLT entry is aligned to a cache line.
-PLT=".plt          ${RELOCATING-0} : ALIGN(16) { *(.plt)${IREL_IN_PLT+ *(.iplt)} }"
